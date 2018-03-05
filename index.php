9c350dcaed382134f522313511cfc5ba0c831af8
marjangalle <36021155+marjangalle@users.noreply.github.com>
2018-03-05 14:31:54 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+require_once 'config.inc.php';
+require 'inc/header.inc.php';
+?>
+<nav class="navbar navbar-expanded navbar-dark bg-dark fixed-top">
+  <span class="navbar-toggler-icon"></span>
+  <a href="#" class="navbar-brand">ABC Shop</a>
+  <ul class="navbar-nav">
+    <li class="nav-item"><a href="#" class="nav-link">Rs 5100.00</a></li>
+  </ul>
+</nav>
+<div class="container-fluid xbody">
+  <div class="row">
+    <div class="col-sm-3 xitem-card" role="button">
+      <div class="card">
+        <div class="card-header">
+          <h6 class="">Marina Vegiteble Oil 500ml</h6>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-3 xitem-card" role="button">
+      <div class="card">
+        <div class="card-header">
+          <h6 class="">Marina Vegiteble Oil 500ml</h6>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-3 xitem-card" role="button">
+      <div class="card">
+        <div class="card-header">
+          <h6 class="">Marina Vegiteble Oil 500ml</h6>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php require_once 'inc/footer.inc.php';?>
